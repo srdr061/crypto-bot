79bc42a4f4bde35b4b4a5c7f9559c9ba77138f17
srdr061 <gpt.srdr@gmail.com>
2024-12-05 22:23:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def get_top_coins():
     try:
         url = 'https://api1.binance.com/api/v3/ticker/price'
         response = requests.get(url)
+        data = response.json()
+        
         if response.status_code == 200:
-            data = response.json()
             usdt_pairs = []
             for coin in data:
                 if isinstance(coin, dict) and coin['symbol'].endswith('USDT'):
@@ -27,7 +28,8 @@ def get_top_coins():
                         'price': float(coin['price'])
                     })
             return jsonify(usdt_pairs[:10])
-        return {"status": "success", "data": data}
+        
+        return {"status": "error", "message": "API request failed"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
 
