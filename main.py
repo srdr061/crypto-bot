632528166c4d612e9348604b68013d20aac937f8
srdr061 <gpt.srdr@gmail.com>
2024-12-05 21:18:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from binance.client import Client
+import time
+
+def main():
+    print("Binance Service Started")
+    while True:
+        print("Service running...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
