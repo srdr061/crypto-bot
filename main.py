194233f912cd41f6b4873e785e0ca844809437a7
srdr061 <gpt.srdr@gmail.com>
2024-12-05 21:37:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from binance.client import Client
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Binance Service is running!"
+    return "Hello from Binance Service!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
