f0849f120f86212e1a9cb3d3d2f2e703fa921a08
srdr061 <gpt.srdr@gmail.com>
2024-12-05 21:54:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from binance.client import Client
 import os
 
 app = Flask(__name__)
-client = Client(None, None)  # Halka açık veriler için API key gerekmiyor
+client = Client(None, None, testnet=True)  # Test modu aktif
 
 @app.route('/')
 def home():
@@ -11,8 +11,11 @@ def home():
 
 @app.route('/price/btc')
 def get_btc_price():
-    btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
-    return jsonify(btc_price)
+    try:
+        btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
+        return jsonify(btc_price)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
