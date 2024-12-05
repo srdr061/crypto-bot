ce779f3647e827b0761bc7fc2fed0131eb131c50
srdr061 <gpt.srdr@gmail.com>
2024-12-05 21:42:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from Binance Service!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
